d203867e9c4827d766eb9dfac11c0785970e0c92
stardust85 <stardust1985@gmail.com>
2015-05-15 20:19:49 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,24 @@
 from setuptools import setup
 
 setup(name='repositorytools',
-      description='Repository tools',
-      packages=['repositorytools'],
-      scripts=['scripts/artifact', 'scripts/repo'],
+      description='Tools for working with artifact repositories',
+      author='Michel Samia',
+      author_email='stardust1985@gmail.com',
+      url='https://github.com/stardust85/repositorytools',
+      license='Apache 2.0',
       platforms='any',
+
+      classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ),
+
+        packages=['repositorytools'],
+        scripts=['scripts/artifact', 'scripts/repo'],
 )
